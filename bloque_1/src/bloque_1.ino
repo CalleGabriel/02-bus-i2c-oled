24e25932f67a9ba0e4d2ce09f4944c357f64d65d
Juan Calle <gabriel.callebarroszz@gmail.com>
2026-09-14 21:22:40 -0500

    feat(Secundarios):"Cambios Secundarios para vizualizar la simulacion"

--- bloque_1/src/bloque_1.ino
+++ bloque_1/src/bloque_1.ino
@@ -20,15 +20,9 @@ void setup() {
     Serial.println("========================================================");
 
     // TODO 1.1: Inicializar el bus I2C en los pines SDA y SCL del ESP32.
-    // Pregunta Guía: ¿Qué función de la librería Wire recibe (SDA_PIN, SCL_PIN)?
-    // Pista: Usa Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
-    /* ESCRIBE TU CÓDIGO AQUÍ */
     Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
 
     // TODO 1.2: Configurar la velocidad del reloj a 400kHz (Modo Rápido).
-    // Pregunta Guía: ¿Qué función configura la frecuencia de reloj del bus I2C?
-    // Pista: Usa Wire.setClock(I2C_CLOCK_SPEED);
-    /* ESCRIBE TU CÓDIGO AQUÍ */
     Wire.setClock(I2C_CLOCK_SPEED);
 
     Serial.println("[I2C] Bus configurado en SDA:GPIO21, SCL:GPIO22 a 400kHz.\n");
@@ -39,15 +33,11 @@ void setup() {
     // TODO 1.3: Recorrer las direcciones válidas de 7 bits (del 1 al 126).
     for (byte address = 1; address < 127; address++) {
 
-        // TODO 1.4: Tocar la puerta del periférico:
-        // 1. Iniciar transmisión con Wire.beginTransmission(address);
-        // 2. Finalizar transmisión capturando el byte de error: byte error = Wire.endTransmission();
-        /* ESCRIBE TU CÓDIGO AQUÍ */
-
+        // TODO 1.4: Tocar la puerta del periférico
         Wire.beginTransmission(address);
         byte error = Wire.endTransmission();
-        // TODO 1.5: Evaluar la respuesta del periférico:
-        // Pregunta Guía: ¿Qué valor devuelve Wire.endTransmission() cuando el periférico responde con ACK (Presente)?
+
+        // TODO 1.5: Evaluar la respuesta del periférico
         if (error == 0) {
             Serial.printf("[I2C] Dispositivo detectado en: 0x%02X ", address);
             if (address == OLED_I2C_ADDR) {
@@ -57,24 +47,16 @@ void setup() {
             }
             encontrados++;
         }
-<<<<<<< HEAD
     }
+
     if (encontrados == 0) {
         Serial.println("[I2C] ❌ No se detectaron dispositivos en el bus.");
         Serial.println("[DIAGNÓSTICO] Revisa que SDA esté en GPIO21, SCL en GPIO22, 3V3 y GND.");
     } else {
-        Serial.printf("\n--- BARRIDO FINALIZADO: %d dispositivo(s) encontrado(s) ---\n", devicesFound);
-=======
->>>>>>> d181c3a6d4e330c39b88cd9034f6614823ac6436
+        Serial.printf("\n--- BARRIDO FINALIZADO: %d dispositivo(s) encontrado(s) ---\n", encontrados);
     }
-   
 }
 
 void loop() {
-    // El escaneo de hardware se ejecuta una sola vez al encender.
     delay(1000);
-<<<<<<< HEAD
-   } 
-=======
 }
->>>>>>> d181c3a6d4e330c39b88cd9034f6614823ac6436
